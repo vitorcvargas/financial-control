eae37e9ac6f84d91243f81d7cadc471cc6a70dd6
vicardoso <vicardoso@uolinc.com>
2022-10-11 13:45:15 -0300

    Created financial info service

---
+++ financial-info/src/test/java/com/financialctl/financialinfo/FinancialInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialctl.financialinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
