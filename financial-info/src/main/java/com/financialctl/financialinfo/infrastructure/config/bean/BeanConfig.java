5c43d5c266633becc10a2ca18d6172ca36b0e3a5
vitorcvargas <vitorcvargas@gmail.com>
2022-10-26 17:38:46 -0300

    Cleaned up bean config

--- financial-info/src/main/java/com/financialctl/financialinfo/infrastructure/config/bean/BeanConfig.java
+++ financial-info/src/main/java/com/financialctl/financialinfo/infrastructure/config/bean/BeanConfig.java
@@ -1,16 +1,30 @@
 package com.financialctl.financialinfo.infrastructure.config.bean;
 
 import com.financialctl.financialinfo.application.ports.inbound.FinanceServicePort;
+import com.financialctl.financialinfo.application.ports.outbound.repositories.FinanceRepository;
 import com.financialctl.financialinfo.application.ports.outbound.repositories.OperationRepository;
+import com.financialctl.financialinfo.application.ports.outbound.repositories.UserRepository;
+import com.financialctl.financialinfo.domain.services.FinanceService;
 import com.financialctl.financialinfo.domain.services.OperationService;
+import com.financialctl.financialinfo.domain.services.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OperationBeanConfig {
+public class BeanConfig {
+
+    @Bean
+    FinanceService financeService(final FinanceRepository financeRepository) {
+        return new FinanceService(financeRepository);
+    }
 
     @Bean
     OperationService operationService(final OperationRepository operationRepository, final FinanceServicePort financeService) {
         return new OperationService(operationRepository, financeService);
+    }
+
+    @Bean
+    UserService userService(final UserRepository userRepository) {
+        return new UserService(userRepository);
     }
 }
