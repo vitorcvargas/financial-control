477114748f60bc520d7858fa6ecb0bcd0e47dbc0
vitorcvargas <vitorcvargas@gmail.com>
2022-11-05 12:08:04 -0300

    Created financial-alert microservice

---
+++ financial-alert/src/main/java/com/financialctl/financialalert/FinancialAlertApplication.java
@@ -0,0 +1,13 @@
+package com.financialctl.financialalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialAlertApplication.class, args);
+	}
+
+}
---
+++ financial-alert/src/test/java/com/financialctl/financialalert/FinancialAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialctl.financialalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
